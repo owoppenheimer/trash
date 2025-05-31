ee226e13a7369aae34ac270bf43089574d7bfbf0
owoppenheimer <naturalotvet@gmail.com>
2025-05-31 17:07:38 +0500

    types.h: added f32 typedef

--- include/types.h
+++ include/types.h
@@ -1,9 +1,11 @@
 #pragma once
-
 #ifndef TYPES_H
 #define TYPES_H
 
+/* It'll be used only for expressions where you need to put <type> many times */
 typedef double f64;
+typedef float f32;
+
 typedef struct Point {
    f64 x, y, z;
 } Point;
