90dd0c9346e8882feae495c01cbf0a60b42b2c1f
owoppenheimer <naturalotvet@gmail.com>
2025-05-31 17:08:47 +0500

    types.h: added f32

--- include/types.h
+++ include/types.h
@@ -2,7 +2,7 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-/* It'll be used only for expressions where you need to put <type> many times */
+/* This'll be used only for expressions where you need to put <type> many times */
 typedef double f64;
 typedef float f32;
 
